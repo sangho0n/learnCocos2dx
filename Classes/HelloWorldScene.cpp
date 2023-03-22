4501cdcb213016b61460881f1e933b26d7972540
sanghoon <54069713+sangho0n@users.noreply.github.com>
2023-03-22 23:17:00 +0900

    4장까지 squash (#1)
    * feat : gorssini 기본 이미지 추가
    
    앵커포인트 실습
    
    * feat : add gorssini's sister
    
    * feat : 여자가 뒤로 오도록
    
    * feat : 여자가 다시 앞으로 오도록
    
    * feat : sprite property 사용
    
    * feat : player 위에 HP 바 스프라이트 추가
    
    * feat : 시스템폰트 레이블 3개 만들기
    
    3.x 버전과 달리 4.0 버전의 Label::create 메서드는 파라미터가 0개임
    
    * modify : 레이블 2 텍스트 변경
    
    바뀐 텍스트는 앞과 뒤가 잘려서 화면에 출력되어야 함
    
    * modify : 레이블 자동줄바꿈
    
    * feat : 비트맵 폰트 사용하기
    
    비트맵폰트를 사용하려면 동일한 이름의 fnt 파일과 png 파일이 있어야 함
    
    * resource : 캐릭터맵 폰트 추가
    
    * feat : png 캐릭터맵 사용하기
    
    png로 주어질 경우 너비, 높이, 시작 아스키코드 문자를 지정해줘야 함.
    
    * modify : plist 캐릭터맵 사용하기
    
    plist 파일은 캐릭터맵 xml 파일로, 사용할 캐릭터맵 이미지와 너비, 높이, 시작문자가 이미 지정되어 있다.
    
    * feat : 트루타입 폰트 사용하기

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,99 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    return HelloWorld::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+    auto wlayer = LayerColor::create(Color4B(125, 125, 125, 255));
+    this->addChild(wlayer);
+
+    TTFConfig ttfConf("fonts/Thonburi.ttf", 40.0f);
+
+    auto pLabel1 = Label::createWithTTF(ttfConf, "Normal");
+    auto pLabel2 = Label::createWithTTF(ttfConf, "Glow");
+    Label* pLabel3;
+    auto pLabel4 = Label::createWithTTF(ttfConf, "Shadow");
+
+    pLabel1->setPosition(Vec2(240, 160 + 90));
+    pLabel1->setColor(Color3B::WHITE);
+
+    pLabel2->setPosition(Vec2(240, 160 + 30));
+    pLabel2->setColor(Color3B::GREEN);
+    pLabel2->enableGlow(Color4B::YELLOW);
+
+    ttfConf.outlineSize = 2;
+    ttfConf.bold = true;
+    pLabel3 = Label::createWithTTF(ttfConf, "Outline");
+    pLabel3->setPosition(Vec2(240, 160 - 30));
+    pLabel3->setColor(Color3B::WHITE);
+    pLabel3->enableOutline(Color4B::BLUE);
+
+    pLabel4->setPosition(Vec2(240, 160 - 90));
+    pLabel4->setColor(Color3B::RED);
+    pLabel4->enableShadow(Color4B::BLACK);
+
+    this->addChild(pLabel1);
+    this->addChild(pLabel2);
+    this->addChild(pLabel3);
+    this->addChild(pLabel4);
+
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
